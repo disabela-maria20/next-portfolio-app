7d39041a598e3c2d6895765d07606d1bb0d0b83e
Isabela Maria <isabela10014@hotmail.com>
2023-04-20 15:46:18 -0300

    build: :truck: update cros job

--- src/pages/api/devjobs.ts
+++ src/pages/api/devjobs.ts
@@ -1,7 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import projects from '../../../utils/api/json/ServerProjescts.json'
+import error from 'next/error';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json(projects);
+  res.status(400).send("Invalid server response")
 }
