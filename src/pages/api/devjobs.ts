e6a0014fa574cb04f7d3b8e5886ebdf13298ac69
Isabela Maria <isabela10014@hotmail.com>
2023-04-20 16:02:47 -0300

    feat: :zap: new test

--- src/pages/api/devjobs.ts
+++ src/pages/api/devjobs.ts
@@ -1,9 +1,6 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
 import projects from '../../../utils/api/json/ServerProjescts.json'
-import error from 'next/error';
+import { NextResponse } from 'next/server';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(projects);
-  res.status(400).send("Invalid server response")
+export async function GET() {
+  return NextResponse.json(projects)
 }
