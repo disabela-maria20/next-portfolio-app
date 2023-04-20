59b8aaf310f980d313c0806eaf100a9388093540
Isabela Maria <isabela10014@hotmail.com>
2023-04-20 18:51:45 -0300

    feat: :rocket: build test

--- src/pages/api/projects/devjobs.ts
+++
@@ -1,6 +0,0 @@
-import projects from '../../../../utils/api/json/ServerProjescts.json'
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  return NextResponse.json(projects)
-}
