4f8e324630e9ba6d09a8bdfd165723df24be4854
Isabela Maria <isabela10014@hotmail.com>
2023-04-25 17:46:59 -0300

    feat: add meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,10 @@ import Script from 'next/script'
 export default function Document() {
   return (
     <Html lang="pt-BR">
+      <link rel="shortcut icon" type="image/x-icon" href="img/icone.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="theme-color" content="#18181b" />
+      <meta name="keywords " content="Desenvolvedora Front-end, Desenvolvimento de Front-end, React.js, Desenvolvimento com React.js, Next.js, Desenvolvimento com Next.js, Typescript, Desenvolvimento com Typescript, SOLID, Programação reativa, Performance, Mobile First, Atomic Design, UI Design, UX Design, Desenvolvimento de UI, Controle de versão, Git, Gerenciamento de código-fonte, Wordpress, Desenvolvimento com Wordpress, HTML5, Desenvolvimento com HTML5, CSS3, Desenvolvimento com CSS3, Javascript, Desenvolvimento com Javascript, Jest, Testes automatizados, Redux, Desenvolvimento com Redux, SQL Server, Banco de dados SQL Server, Otimização de sites, Desempenho de sites, Desenvolvimento de layouts responsivos, Desenvolvimento de peças gráficas, Suporte técnico, Testes de usabilidade, Testes de qualidade, Reuniões de desenvolvimento, Ideias para melhorar a experiência do usuário, Qualidade de projetos, Mulher, Mulher dev, Mulher desenvolvedera, Mulher front-end" />
       <Head />
       <body>
         <Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,34 @@
 import { THome } from '@/components/templates'
+import { NextSeo } from 'next-seo'
 import React from 'react'
 
 const Index = () => {
   return (<>
+    <NextSeo
+      title="Desenvolvedora Front-end React.js, Next.js & Typescript | Isabela M. Ribeiro"
+      description="Conheça a desenvolvedora front-end especializada em programação reativa com React.js e Next.js. Com experiência em SOLID, Mobile First e Atomic Design, já entregou produtos de alta qualidade no mercado."
+      canonical="https://isabelamribeiro.com.br/"
+      openGraph={{
+        url: 'https://isabelamribeiro.com.br/',
+        title: 'Desenvolvedora Front-end React.js, Next.js & Typescript | Isabela M. Ribeiro',
+        description: 'Conheça a desenvolvedora front-end especializada em programação reativa com React.js e Next.js. Com experiência em SOLID, Mobile First e Atomic Design, já entregou produtos de alta qualidade no mercado.',
+        images: [
+          {
+            url: 'https://isabelamribeiro.com.br/img/openGraph-seo.webp',
+            width: 1200,
+            height: 630,
+            alt: 'Desenvolvedora Front-end React.js, Next.js & Typescript'
+          },
+          {
+            url: 'https://isabelamribeiro.com.br/img/img-link-seo.webp',
+            width: 800,
+            height: 600,
+            alt: 'Desenvolvedora Front-end React.js, Next.js & Typescript'
+          }
+        ],
+        site_name: 'Seu Site'
+      }}
+    />
     <THome />
   </>)
 }
