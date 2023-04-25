7bc81a5d44a893a6d2798c72d98436a99a7c76f8
Isabela Maria <isabela10014@hotmail.com>
2023-04-25 17:58:10 -0300

    feat: :sparkles: add  analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,12 +8,23 @@ export default function Document() {
       <link rel="shortcut icon" type="image/x-icon" href="img/icone.ico" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="theme-color" content="#18181b" />
-      <meta name="keywords " content="Desenvolvedora Front-end, Desenvolvimento de Front-end, React.js, Desenvolvimento com React.js, Next.js, Desenvolvimento com Next.js, Typescript, Desenvolvimento com Typescript, SOLID, Programação reativa, Performance, Mobile First, Atomic Design, UI Design, UX Design, Desenvolvimento de UI, Controle de versão, Git, Gerenciamento de código-fonte, Wordpress, Desenvolvimento com Wordpress, HTML5, Desenvolvimento com HTML5, CSS3, Desenvolvimento com CSS3, Javascript, Desenvolvimento com Javascript, Jest, Testes automatizados, Redux, Desenvolvimento com Redux, SQL Server, Banco de dados SQL Server, Otimização de sites, Desempenho de sites, Desenvolvimento de layouts responsivos, Desenvolvimento de peças gráficas, Suporte técnico, Testes de usabilidade, Testes de qualidade, Reuniões de desenvolvimento, Ideias para melhorar a experiência do usuário, Qualidade de projetos, Mulher, Mulher dev, Mulher desenvolvedera, Mulher front-end" />
+      <meta name="keywords " content="Desenvolvedora Front-end, Desenvolvimento de Front-end, React.js, Desenvolvimento com React.js, Next.js, Desenvolvimento com Next.js, Typescript, Desenvolvimento com Typescript, SOLID, Programação reativa, Performance, Mobile First, Atomic Design, UI Design, UX Design, Desenvolvimento de UI, Controle de versão, Git, Gerenciamento de código-fonte, Wordpress, Desenvolvimento com Wordpress, HTML5, Desenvolvimento com HTML5, CSS3, Desenvolvimento com CSS3, Javascript, Desenvolvimento com Javascript, Jest, Testes automatizados, Redux, Desenvolvimento com Redux, SQL Server, Banco de dados SQL Server, Otimização de sites, Desempenho de sites, Desenvolvimento de layouts responsivos, Desenvolvimento de peças gráficas, Suporte técnico, Testes de usabilidade, Testes de qualidade, Reuniões de desenvolvimento, Ideias para melhorar a experiência do usuário, Qualidade de projetos, Mulher, Mulher dev, Mulher desenvolvedera, Mulher front-end, portifolio dev front-end" />
       <Head />
       <body>
         <Main />
         <NextScript />
         <Script strategy="lazyOnload" src={`https://www.google.com/recaptcha/api.js?render=${process.env.KEY_RECAPTCHA}`} />
+
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Y370T3RYFS"></Script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-Y370T3RYFS', { page_path: window.location.pathname });
+            `,
+          }}
+        />
       </body>
     </Html>
   )
