9eebeacdde1439ecb45945dcdc51f39cd165711d
Isabela M. Ribeiro <isabela10014@hotmail.com>
2024-07-05 11:05:32 -0300

    feat: update site map

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ const nextConfig = {
   env: {
     KEY_RECAPTCHA: '6LdzXrUlAAAAAPpKfD9OhhjjP-ZVyiMnui8vM4yH',
     KEY_RECAPTCHA_PRIVATE: '6LdzXrUlAAAAAFWHr17M9DB3RzLIh5D3LRgphKmr'
-  }
-  // distDir: 'build',
-  // trailingSlash: true,
+  },
+  distDir: 'build',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
